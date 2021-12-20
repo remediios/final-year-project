3c4dc6c21643a7442248e6d54b937416869aab68
MiguelRemedios <1919716@brunel.ac.uk>
2021-12-20 21:07:54 +0000

    New packages, Folder Structure Update, Context API addition

--- front-end/fyp/src/App.js
+++ front-end/fyp/src/App.js
@@ -1,7 +1,31 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LogIn from "./components/forms/LogIn";
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 function App() {
-  return <div> Hello World</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <AppContainer>
+          <Routes>
+            <Route exact path="/" element={<LogIn />} />
+            <Route exact path="/auth/signin" element={<LogIn />} />
+            <Route exact path="/auth/signup" element={<LogIn />} />
+          </Routes>
+        </AppContainer>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
