f575b198d851342c7a34c6df5592ded2a101d473
MiguelRemedios <1919716@brunel.ac.uk>
2021-12-21 00:19:52 +0000

    Margin Function + SignIn and Global Styles improvement

--- front-end/fyp/src/components/forms/SignIn.js
+++ front-end/fyp/src/components/forms/SignIn.js
@@ -1,7 +1,31 @@
 import React from "react";
+import {
+  BoldLink,
+  FormBoxContainer,
+  FormContainer,
+  Input,
+  MutedLink,
+  SubmitButton,
+} from "../../styles/forms/Global";
+import { Margin } from "./Margin";
 
 const SignIn = () => {
-  return <div>Sign In</div>;
+  return (
+    <FormBoxContainer>
+      <FormContainer>
+        <Input type="email" placeholder="Email" />
+        <Input type="password" placeholder="Password" />
+      </FormContainer>
+      <Margin direction="vertical" margin={10} />
+      <MutedLink href="#">Forgot your password?</MutedLink>
+      <Margin direction="vertical" margin="1.6em" />
+      <SubmitButton type="submit">Sign-In</SubmitButton>
+      <Margin direction="vertical" margin="1em" />
+      <MutedLink href="/auth/signup">
+        Don't have an account? <BoldLink href="/auth/signup">Sign-Up</BoldLink>
+      </MutedLink>
+    </FormBoxContainer>
+  );
 };
 
 export default SignIn;
