48f995df247d3aa464aaf541f4a6915300306ac3
MiguelRemedios <1919716@brunel.ac.uk>
2021-12-30 18:35:34 +0000

    Styling characters while writing function + Statistics end + error handling

--- front-end/fyp/src/pages/TypingSpeed.js
+++ front-end/fyp/src/pages/TypingSpeed.js
@@ -1,29 +1,108 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import randomWords from "random-words";
 import { Card, CardContent, Container, Button } from "@mui/material";
 import { Input } from "../styles/forms/Global";
-import { HeaderText, MediumText } from "../styles/texts/Headings";
+import {
+  HeaderText,
+  MediumText,
+  StatsResultsText,
+  StatsText,
+} from "../styles/texts/Headings";
 import { Timer } from "../styles/texts/Global";
 
 const TypingSpeed = () => {
   const MAX_WORDS = 200;
-  const SECONDS = 60;
+  const SECONDS = 10;
   const [words, setWords] = useState([]);
   const [countDown, setCountDown] = useState(SECONDS);
+  const [currentInput, setCurrentInput] = useState([]);
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const [currentCharIndex, setCurrentCharIndex] = useState(-1);
+  const [currentChar, setCurrentChar] = useState("");
+  const [correct, setCorrect] = useState(0);
+  const [incorrect, setIncorrect] = useState(0);
+  const [status, setStatus] = useState("waiting");
+  const inputRef = useRef(null);
 
   const generateWords = () => {
     return new Array(MAX_WORDS).fill(null).map(() => randomWords());
   };
 
   const startTimer = () => {
-    setInterval(() => {
-      setCountDown((prevCountDown) => prevCountDown - 1);
-    }, 1000);
+    if (status !== "finished") {
+      setWords(generateWords());
+      setCurrentWordIndex(0);
+      setCorrect(0);
+      setIncorrect(0);
+      setCurrentCharIndex(-1);
+      setCurrentChar("");
+    }
+
+    if (status !== "started") {
+      setStatus("started");
+      let interval = setInterval(() => {
+        setCountDown((prevCountDown) => {
+          if (prevCountDown === 0) {
+            clearInterval(interval);
+            setStatus("finished");
+            setCurrentInput("");
+            return SECONDS;
+          } else {
+            return prevCountDown - 1;
+          }
+        });
+      }, 1000);
+    }
+  };
+
+  const checkMatch = () => {
+    const wordToCompare = words[currentWordIndex];
+    const doesItMatch = wordToCompare === currentInput.trim();
+    if (doesItMatch) {
+      setCorrect(correct + 1);
+    } else {
+      setIncorrect(incorrect + 1);
+    }
+  };
+
+  const handleKeyDown = ({ keyCode, key }) => {
+    if (keyCode === 32) {
+      checkMatch();
+      setCurrentInput("");
+      setCurrentWordIndex(currentWordIndex + 1);
+      setCurrentCharIndex(-1);
+    } else {
+      setCurrentCharIndex(currentCharIndex + 1);
+      setCurrentChar(key);
+    }
+  };
+
+  const getCharacterClass = (wordIndex, charIndex, char) => {
+    if (
+      wordIndex === currentWordIndex &&
+      charIndex === currentCharIndex &&
+      currentChar &&
+      status !== "finished"
+    ) {
+      if (char === currentChar) {
+        return { background: "rgb(66, 186, 150)" };
+      } else {
+        return { background: "rgb(223, 71, 89)" };
+      }
+    } else {
+      return {};
+    }
   };
 
   useEffect(() => {
     setWords(generateWords);
   }, []);
+
+  useEffect(() => {
+    if (status === "started") {
+      inputRef.current.focus();
+    }
+  }, [status]);
 
   return (
     <>
@@ -45,11 +124,17 @@ const TypingSpeed = () => {
         style={{ width: "50%", display: "flex", marginBottom: "30px" }}
       >
         <Input
+          disabled={status !== "started"}
+          ref={inputRef}
           type="text"
           placeholder="Start typing..."
           style={{ borderRadius: "10px" }}
+          onKeyDown={handleKeyDown}
+          value={currentInput}
+          onChange={(e) => setCurrentInput(e.target.value)}
         />
         <Button
+          disabled={status === "started"}
           variant="contained"
           style={{ marginLeft: "10px", background: "rgb(24, 183, 214)" }}
           onClick={startTimer}
@@ -57,19 +142,43 @@ const TypingSpeed = () => {
           Start
         </Button>
       </Container>
-
-      <Container>
-        <Card sx={{ minWidth: 275, border: "1px solid " }}>
-          <CardContent>
-            {words.map((word, i) => (
-              <>
-                <span>{word} </span>
-                <span> </span>
-              </>
-            ))}
-          </CardContent>
-        </Card>
-      </Container>
+      {status === "started" && (
+        <Container>
+          <Card sx={{ minWidth: 275, border: "1px solid " }}>
+            <CardContent>
+              {words.map((word, i) => (
+                <span key={i}>
+                  <span>
+                    {word.split("").map((char, index) => (
+                      <span
+                        style={getCharacterClass(i, index, char)}
+                        key={index}
+                      >
+                        {char}
+                      </span>
+                    ))}
+                  </span>
+                  <span> </span>
+                </span>
+              ))}
+            </CardContent>
+          </Card>
+        </Container>
+      )}
+      {status === "finished" && (
+        <Container style={{ textAlign: "center", display: "flex" }}>
+          <Container>
+            <StatsText>Words per minute (WPM) : </StatsText>
+            <StatsResultsText>{correct}</StatsResultsText>
+          </Container>
+          <Container>
+            <StatsText>Accuracy : </StatsText>
+            <StatsResultsText>
+              {Math.round((correct / (correct + incorrect)) * 100)} %
+            </StatsResultsText>
+          </Container>
+        </Container>
+      )}
     </>
   );
 };
--- front-end/fyp/src/styles/texts/Headings.js
+++ front-end/fyp/src/styles/texts/Headings.js
@@ -15,3 +15,22 @@ export const MediumText = styled.h4`
   z-index: 10;
   color: #000;
 `;
+
+export const StatsText = styled.h3`
+  font-weight: 500;
+  font-size: 16px;
+  z-index: 10;
+  color: #000;
+  margin-top: 30px;
+  margin-bottom: 10px;
+`;
+
+export const StatsResultsText = styled.h3`
+  font-weight: 500;
+  font-size: 24px;
+  z-index: 10;
+  color: #000;
+  margin-top: 40px;
+  color: rgb(24, 183, 214);
+  margin: 0px 0px 30px 0px;
+`;
