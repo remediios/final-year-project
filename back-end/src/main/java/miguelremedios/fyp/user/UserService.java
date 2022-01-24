a010b73554a34f5849b7914ce999a45f74f7d18d
MiguelRemedios <1919716@brunel.ac.uk>
2022-01-24 23:27:15 +0000

    Student Repository Interface + Dependency Injection on the UserService class

--- back-end/src/main/java/miguelremedios/fyp/user/UserService.java
+++ back-end/src/main/java/miguelremedios/fyp/user/UserService.java
@@ -1,19 +1,20 @@
 package miguelremedios.fyp.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.sql.Timestamp;
-import java.util.Date;
 import java.util.List;
 
 @Service
 public class UserService {
 
-    public List<User> getUsers(){
-        Date date = new Date();
-        return List.of(new User(1L,"VOER1SzcREPKP5xrW4RpyIqr7TY2",
-                "miguelremady@gmail.com",
-                "MiguelRemedios",
-                "",
-                ""));
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
     }
 }
