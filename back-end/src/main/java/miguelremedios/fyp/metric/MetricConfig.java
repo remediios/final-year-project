7cf9ebc59fb51520c8dd33620e6d3cc5aa672c67
MiguelRemedios <1919716@brunel.ac.uk>
2022-03-31 23:21:00 +0100

    Jupyter notebook files and datasets addition for data analysis and some other fixings

--- back-end/src/main/java/miguelremedios/fyp/metric/MetricConfig.java
+++ back-end/src/main/java/miguelremedios/fyp/metric/MetricConfig.java
@@ -11,21 +11,14 @@ public class MetricConfig {
     @Bean
     CommandLineRunner MetricCommandLineRunner(MetricRepository repository) {
         return args -> {
-            Metric metric1 = new Metric("max_ts_result", "Maximum Typing Speed Result");
-            Metric metric2 = new Metric("max_ts_accuracy", "Maximum Typing Speed Accuracy");
-            Metric metric3 = new Metric("avg_ts_result", "Average Typing Speed Result");
-            Metric metric4 = new Metric("avg_ts_accuracy", "Average Typing Speed Accuracy");
-            Metric metric5 = new Metric("ip_address", "Internet Protocol V4 Address");
-            Metric metric6 = new Metric("os", "Operating System");
-            Metric metric7 = new Metric("country", "Country");
-            Metric metric8 = new Metric("country_code", "Country Code");
-            Metric metric9 = new Metric("city", "City");
-            Metric metric10 = new Metric("latitude", "Geolocation Latitude");
-            Metric metric11 = new Metric("longitude", "Geolocation Longitude");
-            Metric metric12 = new Metric("currency", "Currency");
+            Metric metric1 = new Metric("ks_kpt", "Number of keys pressed (chars) per time");
+            Metric metric2 = new Metric("md_ct", "Number of clicks per time");
+            Metric metric3 = new Metric("md_cvt", "Number of cryptocurrencies visited per time");
+            Metric metric4 = new Metric("md_bct", "Number of buttons clicked per time");
+            Metric metric5 = new Metric("dom_pv", "Number of cryptocurrency’s detail page views");
+            Metric metric6 = new Metric("ks_kt", "Typing speed per minute");
 
-            repository.saveAll(List.of(metric1, metric2, metric3, metric4, metric5, metric6, metric7,
-                    metric8, metric9, metric10, metric11, metric12));
+            repository.saveAll(List.of(metric1, metric2, metric3, metric4, metric5, metric6));
         };
     }
 }
--- back-end/src/main/java/miguelremedios/fyp/user_metric/UserMetricConfig.java
+++ back-end/src/main/java/miguelremedios/fyp/user_metric/UserMetricConfig.java
@@ -17,15 +17,8 @@ public class UserMetricConfig {
             UserMetric user1Metric4 = new UserMetric(1,4, "50");
             UserMetric user1Metric5 = new UserMetric(1,5, "50");
             UserMetric user1Metric6 = new UserMetric(1,6, "50");
-            UserMetric user1Metric7 = new UserMetric(1,7, "50");
-            UserMetric user1Metric8 = new UserMetric(1,8, "50");
-            UserMetric user1Metric9 = new UserMetric(1,9, "50");
-            UserMetric user1Metric10 = new UserMetric(1,10, "50");
-            UserMetric user1Metric11 = new UserMetric(1,11, "50");
-            UserMetric user1Metric12 = new UserMetric(1,12, "50");
 
-            repository.saveAll(List.of(user1Metric1,user1Metric2,user1Metric3,user1Metric4,user1Metric5,user1Metric6,user1Metric7,
-                    user1Metric8,user1Metric9,user1Metric10,user1Metric11,user1Metric12));
+            repository.saveAll(List.of(user1Metric1,user1Metric2,user1Metric3,user1Metric4,user1Metric5,user1Metric6));
         };
     }
 }
